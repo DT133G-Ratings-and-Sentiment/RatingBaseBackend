c6499963b963341689fe7381559b07e6e0885cd3
Einar Sandström <eisa1001@student.miun.se>
2022-02-24 08:08:12 +0100

    Change name of entity Reviews to Review

--- src/main/java/com/dt002g/reviewapplication/backend/repositories/ReviewRepository.java
+++ src/main/java/com/dt002g/reviewapplication/backend/repositories/ReviewRepository.java
@@ -1,9 +1,9 @@
 package com.dt002g.reviewapplication.backend.repositories;
 
 
-import com.dt002g.reviewapplication.backend.models.Reviews;
+import com.dt002g.reviewapplication.backend.models.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReviewRepository extends JpaRepository<Reviews, Long> {
+public interface ReviewRepository extends JpaRepository<Review, Long> {
 
 }
